df655ccc846a772011d67ba4881d6726efef7b48
Xenanthropy <connerclere@gmail.com>
2026-06-04 01:53:12 -0500

    Replace list of __aeabi functions with proper addresses
    accidentally populated the __aeabi function list with
    the values from the wrong library

--- src/HLE/HLE_SoftFloat.hpp
+++ src/HLE/HLE_SoftFloat.hpp
@@ -36,27 +36,29 @@ namespace HLE::SoftFloat {
         // Functions to patch:
         /* __aeabi
         | Func Name     | Seg |  Start  | Length  | Locals   |  Arguments
-        __aeabi_uidivmod .text	005357D4	00000016              R	.	.	.	.	.	.	.	.	.
-        __aeabi_idivmod	 .text	00535884	00000012              R	.	.	.	.	.	.	.	.	.
-        __aeabi_cdrcmple .text	00535DA0	0000001C              R	.	.	.	.	.	.	.	.	.
-        __aeabi_cdcmple	 .text	00535DBC	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_dcmpeq	 .text	00535DD0	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_dcmplt	 .text	00535DE4	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_dcmple	 .text	00535DF8	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_dcmpge	 .text	00535E0C	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_dcmpgt	 .text	00535E20	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_frsub	   .text	00535FBC	00000008			        R	.	.	.	.	.	.	.	.	.
-        __aeabi_fadd     .text	00535FC8	00000190			        R	.	.	.	.	.	.	.	.	.
-        __aeabi_cfrcmple .text	00536578	00000010              R	.	.	.	.	.	.	.	.	.
-        __aeabi_cfcmple	 .text	00536588	00000014	00000014    R	.	.	.	.	.	.	.	.	.
-        __aeabi_fcmpeq	 .text	0053659C	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_fcmplt	 .text	005365B0	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_fcmple	 .text	005365C4	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_fcmpge	 .text	005365D8	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_fcmpgt	 .text	005365EC	00000014	00000008    R	.	.	.	.	.	.	.	.	.
-        __aeabi_ldivmod	 .text	005366F0	00000044	00000010    R	.	.	.	.	.	.	.	.	.
-        __aeabi_uldivmod .text	00536734	0000003C	00000010    R	.	.	.	.	.	.	.	.	.
-        __aeabi_ldiv0	   .text	00536770	00000010	00000008    R	.	.	.	.	.	.	.	.	.
+        __aeabi_uidivmod .text	004EF5F0	00000020			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_idivmod	 .text	004EF6EC	00000030			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_drsub	   .text	004EF70C	00000008			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_dadd	   .text	004EF718	000002AC	0000000C		R	.	.	.	.	.	.	.	.	.
+        __aeabi_cdrcmple .text	004EFFCC	0000001C			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_cdcmple  .text	004EFFE8	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_dcmpeq   .text	004EFFFC	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_dcmplt	 .text	004F0010	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_dcmple	 .text	004F0024	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_dcmpge	 .text	004F0038	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_dcmpgt	 .text	004F004C	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_frsub	   .text	004F01E8	00000008			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_fadd	   .text	004F01F4	00000190			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_cfrcmple .text	004F07A4	00000010			        R	.	.	.	.	.	.	.	.	.
+        __aeabi_cfcmple	 .text	004F07B4	00000014	00000014		R	.	.	.	.	.	.	.	.	.
+        __aeabi_fcmpeq	 .text	004F07C8	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_fcmplt	 .text	004F07DC	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_fcmple	 .text	004F07F0	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_fcmpge	 .text	004F0804	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_fcmpgt	 .text	004F0818	00000014	00000008		R	.	.	.	.	.	.	.	.	.
+        __aeabi_ldivmod	 .text	004F091C	00000044	00000010		R	.	.	.	.	.	.	.	.	.
+        __aeabi_uldivmod .text	004F0960	0000003C	00000010		R	.	.	.	.	.	.	.	.	.
+        __aeabi_ldiv0	   .text	004F099C	00000010	00000008		R	.	.	.	.	.	.	.	.	.
 
         softfloat:
 
